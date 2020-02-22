e9afb2f597456ecbe121ea29a5f3024485ee7caa
lixuhui123 <2871726754@qq.com>
2020-02-22 22:36:26 +0800

    一些更新呢

--- 排序/希尔排序.cpp
+++ 排序/希尔排序.cpp
@@ -1,60 +1,58 @@
-#include<stdio.h>
-#include<stdlib.h>
-void shellSort(int arr[], int length)
-{
-
-	int temp = 0;
-	int count = 0;
-	// 根据前面的逐步分析，使用循环处理
-	for (int gap = length / 2; gap > 0; gap /= 2)//步长
-	{
-		for (int i = gap; i < length; i++)//一对一对往后走
-		{
-			// 遍历各组中所有的元素(共gap组，每组有个元素), 步长gap
-			for (int j = i - gap; j >= 0; j -= gap)//一组的前面一个，
-			{
-				// 如果当前元素大于加上步长后的那个元素，说明交换
-				if (arr[j] > arr[j + gap])
-				{
-					temp = arr[j];
-					arr[j] = arr[j + gap];
-					arr[j + gap] = temp;
-				}
-			}
-		}
-	}
-}
-void sort(int arr[], int length)
-{
-	int step,i,j,tmp;
-	for (step=length/2 ; step>0 ; step/=2)
-	{
-		for (i=step;i<length;++i)
-		{
-			for (j=i-step;j>=0;j-=step)
-			{
-				if (arr[j] > arr[j+step])
-				{
-					tmp = arr[j];
-					arr[j] = arr[j + step];
-					arr[j + step] = tmp;
-
-				}
-
-			}
-		}
-	}
-
-}
-
-int main()
-{
-	int arr[6] = { 5,0,1,6,10,2 };
-	sort(arr, 6);
-	for (int i=0;i<6;++i)
-	{
-		printf("%d ", arr[i]);
-	}
-    system("pause");
-    return 0;
-} 
+//#include<stdio.h>
+//#include<stdlib.h>
+//void shellSort(int arr[], int length)
+//{
+//
+//	int temp = 0;
+//	int count = 0;
+//	// 根据前面的逐步分析，使用循环处理
+//	for (int gap = length / 2; gap > 0; gap /= 2)//步长
+//	{
+//		for (int i = gap; i < length; i++)//一对一对往后走
+//		{
+//			// 遍历各组中所有的元素(共gap组，每组有个元素), 步长gap
+//			for (int j = i - gap; j >= 0; j -= gap)//一组的前面一个，
+//			{
+//				// 如果当前元素大于加上步长后的那个元素，说明交换
+//				if (arr[j] > arr[j + gap])
+//				{
+//					temp = arr[j];
+//					arr[j] = arr[j + gap];
+//					arr[j + gap] = temp;
+//				}
+//			}
+//		}
+//	}
+//}
+//void sort(int arr[], int length)
+//{
+//	 //希尔排序
+//	int step, i, j, tmp;
+//	for (step = length / 2; step > 0; step /= 2)
+//	{
+//		for (i=step;i<length;++i)
+//		{
+//			for (j=i-step;j>=0;j-=step)
+//			{
+//				if (arr[j]>arr[j+step])
+//				{
+//					tmp = arr[j];
+//					arr[j] = arr[j + step];
+//					arr[j + step] = tmp;
+//				}
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[6] = { 5,0,1,6,10,2 };
+//	sort(arr, 6);
+//	for (int i=0;i<6;++i)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//    system("pause");
+//    return 0;
+//} 
--- 排序/归并排序.cpp
+++ 排序/归并排序.cpp
@@ -29,7 +29,7 @@
 //			b++;
 //		}
 //		c++;
-//	将两个已经比较好的数据存入临时的空间里面
+//	//将两个已经比较好的数据存入临时的空间里面
 //	}
 //	for (; a <= mid; a++, c++)
 //	{
@@ -39,13 +39,13 @@
 //	{
 //		tmp[c] = src[b];
 //	}
-//	将剩余的数据存到临时数组里面
+//	//将剩余的数据存到临时数组里面
 //	int i = 0;
 //	for (i = 0; i <= end; ++i)
 //	{
 //		src[i] = tmp[i];
 //	}
-//	将临时数组元素存入到原数组当中
+//	//将临时数组元素存入到原数组当中
 //}
 //void MergeSort(int *src, int n)
 //{
@@ -53,10 +53,61 @@
 //	dealMergeSort(src, tmp, 0, n - 1);//传入下标
 //	free(tmp);
 //}	
+//
+//void dealsort(int arr[], int * tmp, int str, int end)
+//{
+//	if (str >= end)
+//	{
+//		return;
+//	}
+//	int mid = (str + end) / 2;
+//	dealsort(arr, tmp, str, mid);
+//	dealsort(arr, tmp, mid + 1, end);
+//
+//	int a = str;
+//	int b = mid + 1;
+//	int c = str;
+//
+//	while (a <= mid && b <= end)
+//	{
+//		if (arr[a] > arr[b])
+//		{
+//			tmp[c] = arr[b];
+//			++b;
+//		}
+//		else
+//		{
+//			tmp[c] = arr[a];
+//			++a;
+//		}
+//		++c;
+//	}
+//	for (; a <= mid; ++a, ++c)
+//	{
+//		tmp[c] = arr[a];
+//	}
+//	for (; b <= end; ++b, ++c)
+//	{
+//		tmp[c] = arr[b];
+//	}
+//	for (int i = 0; i <= end; ++i)//易错点
+//	{
+//		arr[i] = tmp[i];
+//	}
+//}
+//
+//void mergeSort(int arr[], int n)
+//{
+//	int* tmp = (int *)malloc(n * sizeof(int));
+//	dealsort(arr, tmp, 0, n - 1);
+//}
+//
+//
+//
 //int main()
 //{
 //	int arr[10] = { 1,5,6,15,8,26,5,55,9,0 };
-//	MergeSort(arr, 10);
+//	mergeSort(arr, 10);
 //	for (int i=0;i<10;++i)
 //	{
 //		printf("%d ", arr[i]);
