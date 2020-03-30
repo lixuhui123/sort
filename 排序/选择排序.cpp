422890c5c9c0c2a58d4121553281504685401ed1
lixuhui123 <2871726754@qq.com>
2020-03-30 22:20:55 +0800

    一些更新

--- 排序/选择排序.cpp
+++ 排序/选择排序.cpp
@@ -1,44 +1,44 @@
-//#include<stdio.h>
-//#include<stdlib.h>
-//#include <algorithm>
-//void SelectionSort(int arr[], int length)
-//{
-//	int i, j;
-//	int _min, tmp,min_index;
-//	for (i = 0; i < length-1; ++i)
-//	{
-//		_min = arr[i];
-//		//min_index = i;
-//		for (j = i + 1; j < length; ++j)
-//		{
-//			if (arr[j] < _min)
-//			{
-//				_min = arr[j];
-//				min_index = j;
-//			}
-//		}
-//		 
-//		tmp = arr[min_index];
-//		arr[min_index] = arr[i];
-//		arr[i] = tmp;
-//		 
-//		
-//	}
-//}
-//int main()
-//{
-//	int arr[11] = { 5,0,11,123,13,11,61,5,4,3,11};
-//	SelectionSort(arr, 11);
-//	for (int i = 0; i <11; ++i)
-//	{
-//		printf("%d ", arr[i]);
-//	}
-//	system("pause");
-//	return 0;
-//}
-//
-// 
-//
-//
-//
-//
+#include<stdio.h>
+#include<stdlib.h>
+#include <algorithm>
+void SelectionSort(int arr[], int length)
+{
+	int i, j;
+	int _min, tmp,min_index;
+	for (i = 0; i < length-1; ++i)
+	{
+		_min = arr[i];
+		min_index = i;//这个必不可少
+		for (j = i + 1; j < length; ++j)
+		{
+			if (arr[j] < _min)
+			{
+				_min = arr[j];
+				min_index = j;
+			}
+		}
+		 
+		tmp = arr[min_index];
+		arr[min_index] = arr[i];
+		arr[i] = tmp;
+		 
+		
+	}
+}
+int main()
+{
+	int arr[11] = { 5,0,11,123,13,11,61,5,4,3,11};
+	SelectionSort(arr, 11);
+	for (int i = 0; i <11; ++i)
+	{
+		printf("%d ", arr[i]);
+	}
+	system("pause");
+	return 0;
+}
+
+ 
+
+
+
+
